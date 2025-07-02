ebfe987a1970e848099a0ed4887cce6561a84ad4
Aizhu Xue <aizhudeveloper@gmail.com>
2025-07-02 08:10:44 -0400

    restructured pizza to object

--- src/app/api/pizzas/route.js
+++ src/app/api/pizzas/route.js
@@ -1,30 +1,25 @@
-const pizzas = [
-    {
-        type: "margherita",
+const pizzas = {
+    "margherita": {
         facts: "Classic pizza with tomato, mozzarella, and basil, representing the Italian flag's colors.",
         yearOfOrigin: 1889
     },
-    {
-        type: "pepperoni",
+    "pepperoni": {
         facts: "Most popular pizza in the U.S., featuring spicy cured sausage.",
         yearOfOrigin: 1910
     },
-    {
-        type: "neapolitan",
+    "neapolitan": {
         facts: "Originated in Naples, Italy, with a thin, soft, and chewy crust.",
         yearOfOrigin: 1700
     },
-    {
-        type: "chicago deep-dish",
+    "chicago deep-dish": {
         facts: "Thick, layered pizza baked in a deep pan, often with cheese on the bottom.",
         yearOfOrigin: 1943
     },
-    {
-        type: "hawaiian",
+    "hawaiian": {
         facts: "Controversial pizza topped with pineapple and ham, created in Canada.",
         yearOfOrigin: 1962
     }
-];
+};
 
 export async function GET(request) {
     const { searchParams, _ } = new URL(request.url)
