2eafc2b92c1d0f04eeb70edb61335817d5ab7074
Aizhu Xue <aizhudeveloper@gmail.com>
2025-07-02 07:40:04 -0400

    get specified pizza

--- src/app/api/pizzas/route.js
+++ src/app/api/pizzas/route.js
@@ -1,31 +1,42 @@
 const pizzas = [
     {
-        type: "Margherita",
+        type: "margherita",
         facts: "Classic pizza with tomato, mozzarella, and basil, representing the Italian flag's colors.",
         yearOfOrigin: 1889
     },
     {
-        type: "Pepperoni",
+        type: "pepperoni",
         facts: "Most popular pizza in the U.S., featuring spicy cured sausage.",
         yearOfOrigin: 1910
     },
     {
-        type: "Neapolitan",
+        type: "neapolitan",
         facts: "Originated in Naples, Italy, with a thin, soft, and chewy crust.",
         yearOfOrigin: 1700
     },
     {
-        type: "Chicago Deep-Dish",
+        type: "chicago deep-dish",
         facts: "Thick, layered pizza baked in a deep pan, often with cheese on the bottom.",
         yearOfOrigin: 1943
     },
     {
-        type: "Hawaiian",
+        type: "hawaiian",
         facts: "Controversial pizza topped with pineapple and ham, created in Canada.",
         yearOfOrigin: 1962
     }
 ];
 
-export async function GET() {
+export async function GET(request) {
+    const { searchParams, _ } = new URL(request.url)
+    const pizzaType = searchParams.get('type').toLowerCase();
+
+    if (pizzaType) {
+        const pizza = pizzas[pizzaType];
+        return pizza ? Response.json(pizza) : new Response(JSON.stringify({ error: "Rapper not found" }), {
+            status: 404,
+            headers: { "Content-Type": "application/json" },
+        });
+    };
+
     return Response.json(pizzas);
 };
